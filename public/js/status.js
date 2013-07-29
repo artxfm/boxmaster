912d52137bdfa8e1cb7de9478e11b7bd3507260e
Mathias Kolehmainen <mathias@voyagergis.com>
2013-07-28 22:50:55 -0400

    more work on layout

--- public/js/status.js
+++ public/js/status.js
@@ -1,6 +1,7 @@
 function StatusCtrl($scope, socket) {
 
   $scope.boxen = [];
+  $scope.selected = null;
 
   socket.on('hello', function(data) {
     socket.emit('get_status', {});
@@ -13,6 +14,51 @@ function StatusCtrl($scope, socket) {
   $scope.addBoxen = function() {
     $scope.todos.push({text:$scope.todoText, done:false});
     $scope.todoText = '';
+  };
+
+  $scope.rowClass = function(box) {
+    return (box.mute == "yes") ? "error" : "";
+  };
+
+  $scope.muteStateSymbol = function(box) {
+    return (box.mute == "yes") ? "icon-volume-off" : "icon-volume-up";
+  };
+
+  $scope.muteStateDesiredSymbol = function(box) {
+    return (box.desired.mute == "yes") ? "icon-volume-off" : "icon-volume-up";
+  };
+
+  $scope.getAudioState = function(box) {
+    return (box.mute == "yes") ? "disabled" : "enabled";
+  };
+
+  $scope.ledStateClass = function(box) {
+    return (box.led == "on" ) ? "" : "offstate";
+  };
+
+  $scope.ledStateDesiredClass = function(box) {
+    return (box.desired.led == "on" ) ? "" : "offstate";
+  };
+
+  $scope.edit = function(box) {
+    console.log("XXX", box);
+    $scope.selected = box;
+  };
+
+  $scope.selectedTitle = function() {
+    if ($scope.selected) {
+      return $scope.selected.id;
+    } else {
+      return "no selection";
+    }
+  };
+
+  $scope.formEditClass = function() {
+    return ($scope.selected) ? "" : "hidden";
+  };
+
+  $scope.getSelection = function() {
+    return $scope.selected;
   };
 
 }
